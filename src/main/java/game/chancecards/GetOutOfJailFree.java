c703cb717416fd9e74efa3bfba60b1ee5f9c660a
Worsøe <s235111@dtu.dk>
2023-11-17 11:20:38 +0100

    Implement GetOutOfJailFree

--- src/main/java/game/chancecards/GetOutOfJailFree.java
+++ src/main/java/game/chancecards/GetOutOfJailFree.java
@@ -1,5 +1,15 @@
 package game.chancecards;
 
+import game.Player;
+
 public class GetOutOfJailFree implements ChanceCard {
-	// TODO: hwat? how? how do card?
+	@Override
+	public String getDescription() {
+		return "Get out of jail free. Keep this card until you need it.";
+	}
+
+	@Override
+	public void perform(Player player) {
+		player.setHasGetOutOfJailFreeCard(true);
+	}
 }
