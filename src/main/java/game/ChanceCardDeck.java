8f2604bc5229612b2ba3956b0688affb93c8f0ae
Worsøe <s235111@dtu.dk>
2023-11-21 16:51:34 +0100

    Start implementing the chance card deck

---
+++ src/main/java/game/ChanceCardDeck.java
@@ -0,0 +1,45 @@
+package game;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import game.chancecards.*;
+
+public class ChanceCardDeck {
+	private ArrayDeque<ChanceCard> cards;
+	private ArrayList<ChanceCard> discardPile;
+
+	public ChanceCardDeck() {
+		cards = new ArrayDeque<ChanceCard>();
+		discardPile = new ArrayList<ChanceCard>(List.of(
+			new Birthday(),
+			new GetOutOfJailFree(),
+			new Homework(),
+			new MoveBy1OrPickAnotherCard(),
+			new MoveToPromenade(),
+			new MoveToStartAndGetMoney(),
+			new MoveUpTo5Squares(),
+			new SkateParkFreeSquare(),
+			new TooManySweets(),
+			new ColorFreeSquare("Red"),
+			new ColorFreeSquare("Gold"),
+			new ColorFreeSquare("Cyan"),
+			new ColorFreeSquare("Gold", "Green"),
+			new ColorFreeSquare("Pink", "Blue"),
+			new ColorFreeSquare("Cyan", "Red"),
+			new ColorFreeSquare("Brown", "Yellow")
+		));
+		reshuffleDiscardPile();
+	}
+
+	public void reshuffleDiscardPile() {
+		Random random = new Random();
+		int index;
+		for (int i = discardPile.size() - 1; i >= 0; i--) {
+			index = random.nextInt(i);
+			cards.add(discardPile.remove(index));
+		}
+	}
+}
