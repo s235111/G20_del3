7e16c4d7fc62416bcc625288ce63bb2ca447a852
Worsøe <s235111@dtu.dk>
2023-11-21 14:47:13 +0100

    Add getters+setters in ColorFreeSquare

--- src/main/java/game/chancecards/ColorFreeSquare.java
+++ src/main/java/game/chancecards/ColorFreeSquare.java
@@ -45,4 +45,20 @@ public class ColorFreeSquare implements ChanceCard {
 			Bank.payRent(player, square);
 		}
 	}
+
+	public String getColor1() {
+		return color1;
+	}
+
+	public void setColor1(String color1) {
+		this.color1 = color1;
+	}
+
+	public String getColor2() {
+		return color2;
+	}
+
+	public void setColor2(String color2) {
+		this.color2 = color2;
+	}
 }
