fac105afba1764bf8332bef02ec25dec2cea02d5
Worsøe <s235111@dtu.dk>
2023-11-24 11:04:17 +0100

    Add unit test for paying for properties

---
+++ src/test/java/game/PayTest.java
@@ -0,0 +1,40 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+
+public class PayTest {
+	@Test
+	public void testPaying() {
+		Player player1 = new Player("Boat");
+		Player player2 = new Player("Dog");
+		GameController.addPlayer(player1);
+		GameController.addPlayer(player2);
+		player1.setBalance(10);
+		player2.setBalance(10);
+		player1.setPosition(6);
+		player2.setPosition(6);
+		assertEquals(10, player1.getBalance());
+		assertEquals(10, player2.getBalance());
+		assertEquals(6, player1.getPosition());
+		assertEquals(6, player2.getPosition());
+
+		GameBoard board = GameController.getGameBoard();
+		Square square = board.getSquare(7);
+		assertEquals("square", square.getType(), "Expected square 7 to be a property");
+
+		assertNull(square.getOwner());
+		player1.move(1);
+		GameController.handleSquare(player1);
+		assertSame(player1, square.getOwner(), "First player didn't become owner of square");
+		assertEquals(10 - square.getValue(), player1.getBalance(), "First player didn't pay for property");
+
+		player2.move(1);
+		GameController.handleSquare(player2);
+		assertSame(player1, square.getOwner(), "Second player got property from first player");
+		assertEquals(10 - square.getValue(), player2.getBalance(), "Second player didn't pay rent");
+	}
+}
